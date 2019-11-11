dd2003e993bb1a89c66feb75716ed70f5c8b1deb
MSalah <msalah0022@gmail.com>
2019-11-11 15:20:18 +0200

    Added Devtools Extension Middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reducers from "./reducers";
 import App from './components/App';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import middleware from "./middlewares";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, middleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
